a6a872ddf64dd368c99d1d89fcff1ea6c7ca90eb
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-12-29 01:09:06 +0530

    day 01. 2

--- todo-App/backend/index.js
+++ todo-App/backend/index.js
@@ -1,10 +1,30 @@
 const express =require('express');
+const {createTodo}=require('./types')
+const {todo}=require('./db')
 const app=express();
 
 app.use(express.json())
 
 
-app.post("/todo",function(req ,res){
+app.post("/todo",async function(req ,res){
+const payload=req.body
+const verify=createTodo.safeParse(payload)
+if(!verify.success)
+{
+    res.status(411).json({
+        message:"not valid template"
+    })
+    return;
+}
+await todo.create({
+    title:payload.title,
+    description:payload.description,
+    completed:false
+})
+res.json({
+    msg:"Created successfully"
+})
+
 
 })
 
